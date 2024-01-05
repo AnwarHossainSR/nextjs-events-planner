d625831ad8712ecdf3bb6ad53b308d0029bd75bf
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-01-05 09:39:53 +0600

    add new dependencies and update layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import '@/styles/globals.css';
 
-import { Inter } from 'next/font/google';
+import { Inter as FontSans } from "next/font/google";
 
 import MainFooter from '@/components/Footer';
+import { cn } from '@/lib/utils';
 import type { ChildrenProps } from '@/types';
 
 export const metadata = {
@@ -13,18 +14,16 @@ export const metadata = {
   title: 'Next Starter',
 };
 
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  adjustFontFallback: false,
-});
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export default async function RootLayout({ children }: ChildrenProps) {
   return (
     <html lang="en">
       <body
-        className={`${inter.className} h-full flex flex-col justify-between`}
-      >
+        className={cn('h-full flex flex-col justify-between',fontSans.variable)}>
         <section className="flex-1">{children}</section>
         <MainFooter />
       </body>
